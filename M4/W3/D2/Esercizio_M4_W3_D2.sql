3bb07171bda6e9dc3b1d375ae5d9ec7c2feac144
StefanoPorcellini <stefano.porcellini@icloud.com>
2024-07-02 14:07:12 +0200

    Esercizio_M4_W3_D2

---
+++ M4/W3/D2/Esercizio_M4_W3_D2.sql
@@ -0,0 +1,23 @@
+﻿--1
+select * from Products;
+
+--2
+select * from Products where UnitsInStock >= 40;
+
+--3
+select * from Employees where City = 'London';
+
+--4
+select * from Orders order by Freight desc;
+
+--5
+select * from Orders where Freight > 90 and Freight < 120;
+ 
+--6
+select * from Products where CategoryID = 1;
+
+--7
+select * from [Order Details] where Discount <> 0;
+
+--8
+select * from Orders where CustomerID = 'BOTTM' and Freight > 50;
