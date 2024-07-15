3bce37177f23565eaa118a3dc984b2dae6bc4dc5
StefanoPorcellini <stefano.porcellini@icloud.com>
2024-07-15 16:57:11 +0200

    modifiche servizi

--- Esercizio_M5_W1/Services/V1/ClienteService.cs
+++ Esercizio_M5_W1/Services/V1/ClienteService.cs
@@ -1,5 +1,6 @@
 ﻿using Esercizio_M5_W1.Models;
 using System.Data.Common;
+using System.Data.SqlClient;
 
 namespace Esercizio_M5_W1.Services.V1
 {
@@ -24,5 +25,75 @@ namespace Esercizio_M5_W1.Services.V1
             };
         }
 
+        public void Create(Cliente cliente)
+        {
+            var query = "INSERT INTO Clienti (Nome, Tipo, CodiceFiscale, PartitaIva) " +
+                        "VALUES (@Nome, @tipoCliente, @CF, @PIVA) SELECT SCOPE_IDENTITY()";
+            var cmd = GetCommand(query);
+            cmd.Parameters.Add(new SqlParameter("@Nome",  cliente.Nome));
+            cmd.Parameters.Add(new SqlParameter("@tipoCliente", cliente.tipoCliente));
+            cmd.Parameters.Add(new SqlParameter("@CF", cliente.CF));
+            cmd.Parameters.Add(new SqlParameter("@PIVA", cliente.PIVA));
+
+            using var conn = GetConnection();
+            conn.Open();
+            var result  = cmd.ExecuteScalar();
+            if (result == null)
+                throw new Exception("Creazione Cliente non completata");
+            cliente.Id = Convert.ToInt32(result);
+        }
+
+        public void Delete(int Id)
+        {
+            var query = "DELETE FROM Clienti WHERE Id = @Id";
+            var cmd = GetCommand(query);
+            cmd.Parameters.Add(new SqlParameter("Id", Id));
+            using var conn = GetConnection();
+            conn.Open();
+            var result = cmd.ExecuteNonQuery();
+            if (result != 1) throw new Exception("Cliente non eliminato");
+        }
+
+        public IEnumerable<Cliente> GetAll()
+        {
+            var query = "SELECT Nome, Tipo, CodiceFiscale, PartitaIva FROM Clienti";
+            var cmd = GetCommand(query);
+            using var conn = GetConnection();
+            conn.Open();
+            var reader = cmd.ExecuteReader();
+            var listaClienti = new List<Cliente>();
+            while (reader.Read()) listaClienti.Add(Create(reader));
+            return listaClienti;
+        }
+
+        public Cliente GetById(int Id)
+        {
+            var query = "SELECT Nome, Tipo, CodiceFiscale, PartitaIva FROM Clienti " +
+                "WHERE Id = @Id";
+            var cmd = GetCommand(query);
+            cmd.Parameters(new SqlParameter("Id", Id));
+            using var conn = GetConnection();
+            conn.Open();
+            var reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                Cliente cliente = new Cliente()
+                {
+                    Nome = reader.GetString(0),
+                    tipoCliente = reader.GetString(1),
+                    CF = reader.GetString(2),
+                    PIVA = reader.GetString(3)
+                };
+                return cliente;
+            } else
+            {
+                throw new Exception("Cliente non trovato")
+            }
+        }
+
+        public void Update(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Esercizio_M5_W1/Services/V1/IClienteService.cs
+++ Esercizio_M5_W1/Services/V1/IClienteService.cs
@@ -1,7 +1,14 @@
-﻿namespace Esercizio_M5_W1.Services.V1
+﻿using Esercizio_M5_W1.Models;
+
+namespace Esercizio_M5_W1.Services.V1
 {
     public interface IClienteService
     {
+        IEnumerable<Cliente> GetAll();
+        void Create(Cliente cliente);
+        void Update(Cliente cliente);
+        void Delete(int Id);
+        Cliente GetById(int Id);
 
     }
 }
